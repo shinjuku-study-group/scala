5885db30995b7c796d1c248224bfcba75238775e
simezi <yuusuke.matsubara@nssys.co.jp>
2014-05-17 17:09:26 +0900

    俺が松原だ

--- fizzbuzz/FizzBuzz_matsu.scala
+++ fizzbuzz/FizzBuzz_matsu.scala
@@ -1,7 +1,7 @@
 /**
  * Created by simezi on 2014/05/10.
  */
-object FizzBuzz {
+object FizzBuzz_matsu {
   def main(args: Array[String]) {
     def checker(divider: Int)(value: Int) = value % divider == 0
     val checker3 = checker(3) _
@@ -9,12 +9,12 @@ object FizzBuzz {
     val checker15 = checker(15) _
 
     val str = for (i <- 1 to 50) yield {
-        if(checker15(i)) " fizzBuzz "
-        else if(checker3(i))  " fizz "
-      else if(checker5(i)) " buzz "
+      if (checker15(i)) " fizzBuzz "
+      else if (checker3(i)) " fizz "
+      else if (checker5(i)) " buzz "
       else i
     }
 
     println(str.mkString)
-    }
-    }
+  }
+}
